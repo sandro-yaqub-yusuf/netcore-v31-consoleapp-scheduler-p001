8194f8df50539876bfa28d3de67d309cb4973b7e
Sandro Yaqub Yusuf <sandroyy@gmail.com>
2022-12-22 16:55:14 -0300

    Criação do projeto
    Criação do projeto

---
+++ README.md
@@ -0,0 +1,22 @@
+# DotNet Core versão 3.1 com C# - Console Application - SCHEDULER - Projeto 001
+
+* Linguagem principal: C# (CSharp)
+* Frameworks utilizadas: DotNet Core versão 3.1
+* Outras linguagens: nenhum
+* Pacotes extras utilizadas: nenhum
+* Banco de Dados utilizado: nenhum
+* Template utilizado: nenhum
+* Editor utilizado: Visual Studio 2019
+* Informações extras: Precisa ter conhecimentos avançados em DOT.NET Core v3.1 (C#).
+
+----
+
+## Descrição
+
+Pequeno projeto em DotNet Core v3.1 (C# com Console Application) para demonstrar o uso com SCHEDULER (agendador de tarefas).
+
+----
+
+## Sobre o Autor
+
+SANDRO YAQUB YUSUF - Analista & Programador Sênior FULL-STACK - Trabalha com desenvolvimento de softwares desde 1990, passando pelas linguagens COBOL, CLIPPER, VISUAL BASIC 6, ASP Clássico, ASP.NET Framework, ASP.NET Core, PHP (Laravel) e NodeJS. Possui conhecimentos em banco de dados como SQL-SERVER, ORACLE, MySQL, MariaDB, MongoDB e SQLite. Também possui conhecimentos em HTML5, CSS3, TYPESCRIPT e JAVASCRIPT. Para as frameworks de desenvolvimento de FRONT-END, possui conhecimentos em ANGULAR, VUEJS e REACT JS. Pratica o modelo CLEAN ARCHITECTURE usando os conhecimentos em DDD, SOLID, TDD e LGPD.
---
+++ src/KITAB.Scheduler.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KITAB.Scheduler.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando a SCHEDULER...");
+            Console.WriteLine("");
+            Console.WriteLine("Serão criadas 4 tarefas:");
+            Console.WriteLine("1. Executar a partir das 13:00 hs com chamadas a cada 10 segundos");
+            Console.WriteLine("2. Executar a partir das 15:15 hs com chamadas a cada 1 minuto");
+            Console.WriteLine("3. Executar a partir das 17:30 hs com chamadas a cada 1 hora");
+            Console.WriteLine("4. Executar a partir das 19:45 hs com chamadas a cada 1 dia");
+            Console.WriteLine("");
+
+            // For Interval in Seconds 
+            // IntervalInSeconds(start_hour, start_minute, seconds)
+            MyScheduler.IntervalInSeconds(13, 00, 10, () => 
+            {
+                Console.WriteLine("Tarefa disparada as 13:00 hs com chamadas a cada 10 segundos");
+            });
+
+            // For Interval in Minutes 
+            // IntervalInMinutes(start_hour, start_minute, minutes)
+            MyScheduler.IntervalInMinutes(15, 15, 1, () => 
+            {
+                Console.WriteLine("Tarefa disparada as 15:15 hs com chamadas a cada 1 minuto");
+            });
+
+            // For Interval in Hours 
+            // IntervalInHours(start_hour, start_minute, hours)
+            MyScheduler.IntervalInHours(17, 30, 1, () => 
+            {
+                Console.WriteLine("Tarefa disparada as 17:30 hs com chamadas a cada 1 hora");
+            });
+
+            // For Interval in Days
+            // IntervalInDays(start_hour, start_minute, days)
+            MyScheduler.IntervalInDays(19, 45, 1, () => 
+            {
+                Console.WriteLine("Tarefa disparada as 19:45 hs com chamadas a cada 1 dia");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
